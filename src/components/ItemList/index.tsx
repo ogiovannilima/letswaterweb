c21814a1b289b01f69165ecefcdd5c9bfa87ddb6
Giovanni Lima <giovannilimadev@gmail.com>
2023-09-18 20:30:54 -0300

    remove page

--- src/components/ItemList/index.tsx
+++ src/components/ItemList/index.tsx
@@ -1,32 +1,11 @@
-import React, { FormEvent, useState } from 'react'
-import { BsCapslock } from 'react-icons/bs'
+import React from 'react'
 
 import { ContainerItem } from './styles'
 
-interface ListTask {
-  task: string
-}
-
 export const ItemList: React.FC = () => {
-  // eslint-disable-next-line no-unused-vars
-  const [listTask, setListTask] = useState<ListTask[]>([])
-  const [task, setTask] = useState('')
-
-  function handleAddTask(event: FormEvent) {
-    event.preventDefault()
-    console.log('add task=', listTask)
-  }
-
   return (
-    <ContainerItem onSubmit={handleAddTask}>
-      <input
-        type="text"
-        value={task}
-        onChange={(event) => setTask(event.target.value)}
-      />
-      <button type="submit">
-        <BsCapslock />
-      </button>
+    <ContainerItem>
+      <h1>alo</h1>
     </ContainerItem>
   )
 }
